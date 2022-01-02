71c87c1788a6e10a5d955da9a2899e962b961e92
Eltonite <eltoniteonblockchain@gmail.com>
2022-01-02 02:46:30 -0600

    Add basic react app and 3 temp buttons

---
+++ bbcf-labbing/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className= 'flex justify-center gap-4 px-2 py-2'>
+      <button className='bg-emerald-500 px-3 text-2xl font-sans rounded-lg'>GGST</button>
+      <button className='bg-blue-400 px-3 text-2xl font-sans rounded-lg'>BBCF</button>
+      <button className='bg-orange-400 px-3 text-2xl font-sans rounded-lg'>DBFZ</button>
+    </div>
+  );
+}
+
+export default App;

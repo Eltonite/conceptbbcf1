cf8a57ed2c917ffe50f8227ff1380c619af0948a
Eltonite <eltoniteonblockchain@gmail.com>
2022-01-04 04:19:57 -0600

    Change BrowserRouter to HashRouter

--- bbcf-labbing/src/App.js
+++ bbcf-labbing/src/App.js
@@ -1,6 +1,6 @@
 import Homedev from "./Homedev";
 import Navbar from "./Navbar";
-import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import {HashRouter as Router, Routes, Route} from 'react-router-dom';
 import Home from "./Home";
 import Homebbcf from "./Homebbcf";
 import EsHome from "./EsHome";
